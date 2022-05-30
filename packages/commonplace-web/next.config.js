cec9afceab4a578b67ab1f99d8cecc19df3dfb4a
Alex Goodman <alexthegoodman@gmail.com>
2022-05-30 09:35:09 -0400

    disable swc next.js

--- packages/commonplace-web/next.config.js
+++ packages/commonplace-web/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
