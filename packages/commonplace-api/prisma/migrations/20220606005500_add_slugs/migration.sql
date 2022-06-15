c1ed85157dad38a5a2a36c5856558b77f18ba942
Alex Goodman <alexthegoodman@gmail.com>
2022-06-14 21:28:28 -0400

    manual alter migration

--- packages/commonplace-api/prisma/migrations/20220606005500_add_slugs/migration.sql
+++ packages/commonplace-api/prisma/migrations/20220606005500_add_slugs/migration.sql
@@ -9,26 +9,15 @@
   - A unique constraint covering the columns `[generatedModifierSlug]` on the table `Modifier` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX "Category_generatedNameSlug_key";
-
--- DropIndex
-DROP INDEX "Interest_generatedNameSlug_key";
-
--- DropIndex
-DROP INDEX "Modifier_generatedNameSlug_key";
 
 -- AlterTable
-ALTER TABLE "Category" DROP COLUMN "generatedNameSlug",
-ADD COLUMN     "generatedCategorySlug" TEXT NOT NULL DEFAULT E'';
+ALTER TABLE "Category" ADD COLUMN "generatedCategorySlug" TEXT NOT NULL DEFAULT E'';
 
 -- AlterTable
-ALTER TABLE "Interest" DROP COLUMN "generatedNameSlug",
-ADD COLUMN     "generatedInterestSlug" TEXT NOT NULL DEFAULT E'';
+ALTER TABLE "Interest" ADD COLUMN "generatedInterestSlug" TEXT NOT NULL DEFAULT E'';
 
 -- AlterTable
-ALTER TABLE "Modifier" DROP COLUMN "generatedNameSlug",
-ADD COLUMN     "generatedModifierSlug" TEXT NOT NULL DEFAULT E'';
+ALTER TABLE "Modifier" ADD COLUMN "generatedModifierSlug" TEXT NOT NULL DEFAULT E'';
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Category_generatedCategorySlug_key" ON "Category"("generatedCategorySlug");
