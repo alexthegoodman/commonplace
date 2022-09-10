272ab4c76832e6ace8938944c4a2d1e93a131c7d
Alex Goodman <alexthegoodman@gmail.com>
2022-09-10 11:54:23 -0400

    update urls

--- packages/commonplace-utilities/def/urls.ts
+++ packages/commonplace-utilities/def/urls.ts
@@ -1,13 +1,11 @@
 export const env = process.env.NEXT_PUBLIC_APP_ENV;
 
 export const cpDomain =
-  env === "production"
-    ? "commonplaceapi-env.eba-u9h46njg.us-east-2.elasticbeanstalk.com"
-    : "localhost";
+  env === "production" ? "commonplace.social" : "localhost";
 
 export const cpGraphqlUrl =
   env === "production"
-    ? "http://commonplaceapi-env.eba-u9h46njg.us-east-2.elasticbeanstalk.com:4000/graphql"
+    ? "https://commonplace.social:4000/graphql"
     : "http://localhost:4000/graphql";
 
 export const cloudfrontUrl = "https://d3ubks77jdbtp7.cloudfront.net/";
