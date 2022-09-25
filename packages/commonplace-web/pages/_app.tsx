1092e7a36ab606901a0a2bf3f9f4a902d842f244
Alex Goodman <alexthegoodman@gmail.com>
2022-09-25 08:26:34 -0400

    install meta pixel and disable double init

--- packages/commonplace-web/pages/_app.tsx
+++ packages/commonplace-web/pages/_app.tsx
@@ -21,6 +21,8 @@ mixpanel.init("0257a00f77cd9b500e88e34f96b2e991", { debug: isDevelopment });
 // }
 
 var tracked = false;
+var hotjarInitialized = false;
+var pixelInitialized = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const gqlClient = new GQLClient("");
@@ -50,23 +52,56 @@ function MyApp({ Component, pageProps }: AppProps) {
     };
   `;
 
-  const initializeHotjar = `(function(h,o,t,j,a,r){
+  // NOTE: typeof checks for ssr, varIntialized checks once on client
+  const initializeHotjar = `
+    if (typeof hotjarInitialized === "undefined" || !hotjarInitialized) {
+      (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3162621,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
+        hotjarInitialized = true;
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    }
+  `;
+
+  const initializeMetaPixel = `
+    if (typeof pixelInitialized === "undefined" || !pixelInitialized) {
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '606669884534740');
+      fbq('track', 'PageView');
+      pixelInitialized = true;
+    }
+  `;
 
   return (
     <>
       {/* <section className="globalContainer"> */}
       <Head>
         <link rel="stylesheet" href="/globals.min.css" />
+        {/** Meta Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=606669884534740&ev=PageView&noscript=1"
+          />
+        </noscript>
       </Head>
+      {/** TODO: strategy="afterInteractive" ? */}
       <Script dangerouslySetInnerHTML={{ __html: initializeHotjar }} />
       <Script dangerouslySetInnerHTML={{ __html: initializeFacebookSDK }} />
+      <Script dangerouslySetInnerHTML={{ __html: initializeMetaPixel }} />
       <Script async defer src="https://connect.facebook.net/en_US/sdk.js" />
       <Component {...pageProps} />
       {/* </section> */}
