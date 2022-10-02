5d0ffb5058fdb3ea42212f731c04a1e37f1c6a06
Alex Goodman <alexthegoodman@gmail.com>
2022-10-02 19:20:29 -0400

    test use of __dirname

--- packages/commonplace-web/next-i18next.config.js
+++ packages/commonplace-web/next-i18next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     defaultLocale: "en",
     // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     locales: ["en", "bn"],
-    localePath: path.resolve("./public/locales"),
+    localePath: path.resolve(__dirname + "./public/locales"),
   },
 };
