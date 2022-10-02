1ee6e33012017afe069533b4acf597960dedbb70
Alex Goodman <alexthegoodman@gmail.com>
2022-10-02 18:43:57 -0400

    add localePath

--- packages/commonplace-web/next-i18next.config.js
+++ packages/commonplace-web/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
     locales: ["en", "bn"],
+    localePath: path.resolve("./public/locales"),
   },
 };
