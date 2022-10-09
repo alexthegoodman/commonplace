8b2773ebec702baceb4e5aa5dc6884d50edd3e9c
Alex Goodman <alexthegoodman@gmail.com>
2022-10-09 18:32:10 -0400

    header fix

--- packages/commonplace-dashboard/components/ManagementNavigation/ManagementNavigation.tsx
+++ packages/commonplace-dashboard/components/ManagementNavigation/ManagementNavigation.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { ManagementNavigationProps } from "./ManagementNavigation.d";
 
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
 
 const ManagementNavigation: React.FC<ManagementNavigationProps> = ({
@@ -12,31 +11,10 @@ const ManagementNavigation: React.FC<ManagementNavigationProps> = ({
 }) => {
   const clickHandler = (e: MouseEvent) => onClick(e);
   return (
-    <NavigationMenu.Root>
-      <NavigationMenu.List>
-        <NavigationMenu.Item>
-          <Link href="/manage">Manage Posts</Link>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Item>
-          <Link href="/manage/users">Manage Users</Link>
-        </NavigationMenu.Item>
-
-        {/* <NavigationMenu.Item>
-          <NavigationMenu.Trigger />
-          <NavigationMenu.Content>
-            <NavigationMenu.Sub>
-              <NavigationMenu.List />
-              <NavigationMenu.Viewport />
-            </NavigationMenu.Sub>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Indicator /> */}
-      </NavigationMenu.List>
-
-      <NavigationMenu.Viewport />
-    </NavigationMenu.Root>
+    <header>
+      <Link href="/manage">Manage Posts</Link> |
+      <Link href="/manage/users">Manage Users</Link>
+    </header>
   );
 };
 
