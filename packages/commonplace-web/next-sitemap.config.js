0528988d21bf61c276a0a26275a34598284c7d7c
Alex Goodman <alexthegoodman@gmail.com>
2022-10-11 20:51:34 -0400

    update sitemap

--- packages/commonplace-web/next-sitemap.config.js
+++ packages/commonplace-web/next-sitemap.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "http://0.0.0.0:3000" || "https://commonplace.social",
+  siteUrl: "https://commonplace.social",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 5000,
   exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      "http://0.0.0.0:3000/server-sitemap.xml", // <==== Add here
+      "https://commonplace.social/server-sitemap.xml", // <==== Add here
     ],
   },
   // ...other options
