c15c8c34d57e79985d262fa7ff9b428e1060544f
Alex Goodman <alexthegoodman@gmail.com>
2022-10-22 20:13:41 -0400

    new migration

--- packages/commonplace-api/prisma/migrations/20221022233607_favorite_interest/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "favoriteInterestId" TEXT NOT NULL DEFAULT E'';
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_favoriteInterestId_fkey" FOREIGN KEY ("favoriteInterestId") REFERENCES "Interest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- packages/commonplace-api/prisma/migrations/20221023001228_add_favorite_interest/migration.sql
+++ packages/commonplace-api/prisma/migrations/20221023001228_add_favorite_interest/migration.sql
@@ -1,8 +1,5 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_favoriteInterestId_fkey";
-
 -- AlterTable
-ALTER TABLE "User" ALTER COLUMN "favoriteInterestId" DROP NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "favoriteInterestId" TEXT;
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_favoriteInterestId_fkey" FOREIGN KEY ("favoriteInterestId") REFERENCES "Interest"("id") ON DELETE SET NULL ON UPDATE CASCADE;
